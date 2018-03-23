ccb3f8c6609e2453fe1bd1d134e73931e43a4ac6
sanderhelleso <37444808+sanderhelleso@users.noreply.github.com>
2018-03-24 00:55:18 +0100

    Node setup

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const handlebars = require("express-handlebars");
+const ejs = require("ejs");
+
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(`${__dirname}/public`));
+
+// routes
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+
+
+
+server.listen(port, () => {
+	console.log(`Server started on ${port}`);
+});
