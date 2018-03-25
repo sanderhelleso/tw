dd53526a8fe28e4e0e4d265a8f852e916021e1ec
Sanderhelleso <sanderlade@hotmail.com>
2018-03-25 14:33:20 +0200

    added firebase db

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,6 +2,17 @@ window.onload = start;
 
 function start() {
 
+	// init firebase
+	const config = {
+	    apiKey: "AIzaSyA-hRi2HALi-xXdym3J9ov5NcplIgujH7I",
+	    authDomain: "twproject-13f58.firebaseapp.com",
+	    databaseURL: "https://twproject-13f58.firebaseio.com",
+	    projectId: "twproject-13f58",
+	    storageBucket: "",
+	    messagingSenderId: "744116401403"
+	};
+	firebase.initializeApp(config);
+
 	// load modules
     feather.replace()
     particlesJS.load("particles-js", "/scripts/particles.json", function() {
