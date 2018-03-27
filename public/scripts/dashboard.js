6a6310aa887e534be36f6249bc03b43f8d99851b
Sanderhelleso <sanderhelleso@hotmail.com>
2018-03-27 06:16:52 +0200

    Soscial notification toggle added

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,5 +1,11 @@
 window.onload = start;
 
+// GLOBALS USED WITHIN APP
+var accountRef;
+var uidKey;
+var notificationRef;
+var loadNotification;
+var notificationStatus;
 function start() {
 
 	// init firebase
@@ -16,13 +22,22 @@ function start() {
 	// load user
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
-		    var uidKey = user.uid;
-		    var accountRef = firebase.database().ref("accounts/" + uidKey);
+		    uidKey = user.uid;
+		    accountRef = firebase.database().ref("accounts/" + uidKey);
 		    accountRef.once("value", function(snapshot) {
 		    	// set account UI
 		    	document.getElementById("userAccount").innerHTML = "<img id='userAvatar' src='/img/avatar.png' alt='Avatar'> " + snapshot.val().First_Name + " " + snapshot.val().Last_Name + " <i data-feather='chevron-down'></i>";
 		    	// load icon module
 		    	feather.replace();
+
+		    	// load notification status
+		    	loadNotification = true;
+		    	notificationRef = firebase.database().ref("accounts/" + uidKey + "/notifications");
+		    	notificationRef.once("value", function(snapshot) {
+		    		notificationStatus = snapshot.val().Notification_Status;
+		    		toggleNotifications();
+		    	});
+				
 		    	// fade in dashboard
 		    	document.getElementById("loadingCover").classList.add("fadeOut");
 	    		setTimeout(function() {
@@ -39,6 +54,9 @@ function start() {
 
 	// user menu
 	document.getElementById("userAccount").addEventListener("click", openMenu);
+
+	// notifications
+	document.getElementById("notificationsOff").addEventListener("click", toggleNotifications);
 }
 
 // sign out
@@ -67,4 +85,56 @@ function openMenu() {
 	setTimeout(function() {
 		document.getElementById("userAccountMenu").classList.remove("fadeIn");
 	}, 600);
+}
+
+function toggleNotifications() {
+
+	// notification toggle button
+	var notificationsOff = document.getElementById("notificationsOff");
+
+	// if notification is on
+	var path = notificationsOff.childNodes[0].childNodes[0].outerHTML;
+	var line = notificationsOff.childNodes[0].childNodes[1];
+	var notification = document.getElementById("notification");
+	if (notification.classList.contains("feather-bell")) {
+		if (notificationStatus === "off" || notificationStatus === false) {
+			notificationsOff.classList.remove("feather-bell-off");
+			notificationsOff.classList.add("feather-bell");
+			notificationsOff.childNodes[0].childNodes[0].outerHTML = notification.childNodes[0].outerHTML;
+			notificationsOff.childNodes[2].innerHTML = "Turn on notifications";
+
+			notification.classList.remove("feather-bell");
+			notification.classList.add("feather-bell-off");
+			notification.childNodes[0].outerHTML = path;
+			notification.appendChild(line);
+			feather.replace();
+
+			notificationRef.update({
+				Notification_Status: "off"
+			});
+		}
+	}
+
+	// if notification is off
+	else {
+		if (notificationStatus === "on" || notificationStatus === false) {
+			notificationsOff.classList.remove("feather-bell");
+			notificationsOff.classList.add("feather-bell-off");
+			notificationsOff.childNodes[0].childNodes[0].outerHTML = notification.childNodes[0].outerHTML;
+			var line = notification.childNodes[1];
+			notificationsOff.childNodes[0].appendChild(line);
+			notificationsOff.childNodes[2].innerHTML = "Turn off notifications";
+			notification.classList.remove("feather-bell-off");
+			notification.classList.add("feather-bell");
+			notification.childNodes[0].outerHTML = path;
+			feather.replace();
+	
+			notificationRef.update({
+				Notification_Status: "on"
+			});
+		}
+	}
+// controll check on load
+notificationStatus = false;
+loadNotification = false;
 }
