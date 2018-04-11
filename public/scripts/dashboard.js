38ba55e1be0eb77784ce4c27b4f6feea69d6f349
Sanderhelleso <sanderhelleso@hotmail.com>
2018-04-11 03:14:21 +0200

    implemented quill and modified editor

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -3626,6 +3626,7 @@ function openProject() {
 	//document.getElementById("timesheetTrigger").click();
 
 	members();
+	reports();
 	timesheet();
 }
 
@@ -3928,6 +3929,48 @@ function updateRole() {
 	document.getElementById("selectRole").value = "Choose";
 	checkRole();
 }
+
+// display reports and init reports event
+function reports() {
+	// init events
+	document.getElementById("newPreReportCont").addEventListener("click", newPreReport);
+}
+
+// opens a new pre-report modal
+function newPreReport() {
+	$('#newPreReportModal').modal('show');
+	newPreReportEditor();
+}
+
+function newPreReportEditor() {
+	var toolbarOptions = [
+	  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+	  ['blockquote', 'code-block'],
+
+	  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+	  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+	  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+	  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+	  [{ 'direction': 'rtl' }],                         // text direction
+
+	  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+	  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+	  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+	  [{ 'font': [] }],
+	  [{ 'align': [] }],
+
+	  ['clean']                                         // remove formatting button
+	];
+
+	var quill = new Quill('#newPreReportEditor', {
+	  modules: {
+	    toolbar: toolbarOptions
+	  },
+	  theme: 'snow'
+	});
+}
+
 
 // timesheet for project member
 var commitCount = 0;
